22971190ff4b6e4f5b71b19e4f97636fc76ff93d
CaseyKelly <casey.kelly@colorado.edu>
2017-04-02 16:28:29 -0600

    npm install --save youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY='AIzaSyDC7GnqyF77YwjU1nUwG5PZ0xnbTkUs4vYnp'
+
 // Create a new component. This component should produce some HTML.
 const App = () => {
   return <div>Hi!</div>;
